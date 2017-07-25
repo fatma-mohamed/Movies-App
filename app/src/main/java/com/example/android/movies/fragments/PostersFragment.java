e41e5165c6cfb450d8ed816c240af8cd9f54e8f4
fatma-mohamed <fatma.mohmed.001@gmail.com>
2017-07-25 12:20:27 +0200

    Code reorganisation

---
+++ app/src/main/java/com/example/android/movies/fragments/PostersFragment.java
@@ -0,0 +1,145 @@
+package com.example.android.movies.fragments;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.RadioGroup;
+
+import com.example.android.movies.R;
+import com.example.android.movies.adapters.ImageAdapter;
+import com.example.android.movies.data.DatabaseHelper;
+import com.example.android.movies.data.MoviesContract;
+import com.example.android.movies.helpers.DataDisplay;
+import com.example.android.movies.helpers.FetchData;
+import com.example.android.movies.helpers.MovieDetailsParser;
+import com.example.android.movies.helpers.LayoutListener;
+
+import java.util.HashMap;
+
+import static com.example.android.movies.R.id.container;
+
+
+public class PostersFragment extends Fragment implements DataDisplay {
+    private ImageAdapter adapter;
+    public Boolean favShown = false;
+    private LayoutListener layoutListener;
+    public static DatabaseHelper db;
+    private HashMap<String,String>[] movies;
+
+
+    public void setLayoutListener(LayoutListener layoutListener) {
+        this.layoutListener = layoutListener;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        db = new DatabaseHelper(getContext());
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if(id == R.id.action_sort)
+        {
+            final Dialog sort_dialog = new Dialog(getContext());
+            sort_dialog.setContentView(R.layout.sort_dialog);
+            sort_dialog.show();
+            final RadioGroup sort_choice = (RadioGroup) sort_dialog.findViewById(R.id.sort_radio_group);
+            sort_choice.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                    if(checkedId == R.id.popularity_option)
+                        adapter.sortBy("popularity");
+                    else if (checkedId == R.id.top_rated_option)
+                        adapter.sortBy("top_rated");
+                    sort_dialog.cancel();
+                }
+            });
+            return true;
+        }
+        else if(id == R.id.action_show)
+        {
+            final Dialog show_dialog = new Dialog(getContext());
+            show_dialog.setContentView(R.layout.show_dialog);
+            show_dialog.show();
+            final RadioGroup sort_choice = (RadioGroup) show_dialog.findViewById(R.id.show_radio_group);
+            sort_choice.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                    if(checkedId == R.id.fav_option) {
+                        favShown = true;
+                        adapter.showFav();
+                    }
+                    else if (checkedId == R.id.all_option)
+                        adapter.add(movies);
+                    show_dialog.cancel();
+                }
+            });
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_posters, container, false);
+        HashMap<String,String>[] temp = new HashMap[0];
+        adapter = new ImageAdapter(getActivity(),temp);
+        GridView posters_grid  =(GridView) rootView.findViewById(R.id.posters_gridview);
+        posters_grid.setAdapter(adapter);
+        posters_grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String name = String.valueOf(position);
+                layoutListener.set(name);
+            }
+        });
+        return rootView;
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        updatePosters();
+    }
+
+    public void updatePosters()
+    {
+        FetchData moviesTask = new FetchData(getContext(), new MovieDetailsParser(),this, MoviesContract.MOVIE_DB_URL+ MoviesContract.MOVIE_DB_POPULAR);
+        moviesTask.execute();
+    }
+
+
+    public void display(HashMap<String,String>[] results)
+    {
+        if(results!=null)
+        {
+            movies = results;
+            adapter.add(results);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/android/movies/fragments/ReviewsFragment.java
@@ -0,0 +1,67 @@
+package com.example.android.movies.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.android.movies.R;
+import com.example.android.movies.adapters.ReviewAdapter;
+import com.example.android.movies.data.MoviesContract;
+import com.example.android.movies.helpers.DataDisplay;
+import com.example.android.movies.helpers.FetchData;
+import com.example.android.movies.helpers.MovieReviewsParser;
+
+import java.util.HashMap;
+
+/**
+ * Created by fatma on 12/2/2016.
+ */
+
+public class ReviewsFragment extends Fragment implements DataDisplay{
+    private ListView reviews_list;
+    private ReviewAdapter reviewAdapter;
+    private String movie_id;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        movie_id = getArguments().getString("id");
+        View rootView = inflater.inflate(R.layout.fragment_reviews, container, false);
+        reviews_list = (ListView)rootView.findViewById(R.id.reviews_list);
+        HashMap<String,String>[] temp = new HashMap[0];
+        reviewAdapter = new ReviewAdapter(getActivity(),temp);
+        reviews_list.setAdapter(reviewAdapter);
+        return rootView;
+    }
+
+    @Override
+    public void display(HashMap<String, String>[] results) {
+        reviewAdapter.add(results);
+        reviewAdapter.notifyDataSetChanged();
+        if(results.length == 0){
+            Toast.makeText(this.getContext(),"No reviews available",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        updateReviews();
+    }
+
+    public void updateReviews() {
+        FetchData reviewsTask = new FetchData(getContext(), new MovieReviewsParser(), this, MoviesContract.MOVIE_DB_URL + movie_id + MoviesContract.MOVIE_DB_REVIEWS);
+        reviewsTask.execute();
+    }
+}
